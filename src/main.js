d068e3a0721ab34b1d046d353a40ab4eaa2982d9
FEDY <fedychung@gmail.com>
2020-07-08 16:07:25 +0700

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from "vue-router";
 import App from './App.vue'
-import routes from './routes'; 
+import routes from './routes';
+import store from './store';
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,6 +17,7 @@ const router = new VueRouter({
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
