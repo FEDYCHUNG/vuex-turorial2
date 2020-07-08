282abccb33c281b4c1e7f6302f856d7261a56a4c
FEDY <fedychung@gmail.com>
2020-07-08 17:06:20 +0700

    fix product detail

--- src/pages/Product.vue
+++ src/pages/Product.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="col-8">
       <h1>{{ product.title }}</h1>
-      <h3>{{ product.price }}</h3>
+      <h3>$ {{ product.price }}</h3>
       <input type="text" class="text-center col-1 mr-2 p-1" />
       <button class="btn btn-primary">Add to Cart</button>
       <p class="mt-4">
