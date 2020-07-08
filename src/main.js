9f3f00424cbf30822f1d8003abb3fc752dfdf299
FEDY <fedychung@gmail.com>
2020-07-08 12:18:59 +0700

    First Initial , Install Boostrap, Vue-Router

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from "vue-router";
+import App from './App.vue'
+import routes from './routes'; 
+
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: routes,
+  mode: "history"
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
